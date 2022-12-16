2ad37d22ed5b5c5ba7c63020ffbc05d9a4d958d2
iago0310 <iago0310@hotmail.com>
2022-12-16 19:50:12 -0300

    Entrega do jogo "completo"

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,12 +1,11 @@
 import pygame
-
 import random
-from dino_runner.components.obstacles.cactus import small_cactus ##importa a função do cacto pequeno
+
+from dino_runner.components.obstacles.cactus import small_cactus, large_cactus ##importa a função do cacto pequeno
 from dino_runner.components.obstacles.cactus import large_cactus ##importa a função do cacto grande
 from dino_runner.components.obstacles.bird import bird ##importa a função do passaro
-from dino_runner.utils.constants import SMALL_CACTUS ##importa a imagem do cacto pequeno
-from dino_runner.utils.constants import LARGE_CACTUS ##importa a imagem do cacto grande
-from dino_runner.utils.constants import BIRD ##importa a imagem do passaro
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD ##importa a imagem do cacto pequeno, grande, passaro
+
 
 class ObstacleManager:
     def __init__(self):
@@ -14,20 +13,36 @@ class ObstacleManager:
 
     def update(self, game):
         if len(self.obstacles) == 0: ##randomiza entre os 3 obstaculos
-            if random.randint(0, 2) == 0:
+            obs = random.randint(0, 2)
+            if obs == 0:
                 self.obstacles.append(small_cactus(SMALL_CACTUS))
-            elif random.randint(0, 2) == 1:
+            elif obs == 1:
                 self.obstacles.append(large_cactus(LARGE_CACTUS))
-            elif random.randint(0, 2) == 2:
+            elif obs == 2:
                 self.obstacles.append(bird(BIRD))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                pygame.time.delay(1000)
-                game.playing = False
-                break
+                
+                if game.player.type == 'hammer':
+                    self.obstacles.remove(obstacle)
+                    game.game_speed = 20
+
+                elif game.player.type == 'shield':
+                    self.obstacles.remove(obstacle)
+                    
+                else:
+                    game.player.has_power_up
+                    pygame.time.delay(500)
+                    game.playing = False
+                    game.death_count += 1
+                    break    
+
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_obstacles(self):
+        self.obstacles = []           
